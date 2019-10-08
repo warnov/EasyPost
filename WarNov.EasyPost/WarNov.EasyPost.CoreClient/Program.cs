e2d3108726c5ad0fa443c22bc5d390497877ff4f
warnov <warnov@gmail.com>
2019-10-07 20:15:22 -0500

    MVP Ready

---
+++ WarNov.EasyPost/WarNov.EasyPost.CoreClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarNov.EasyPost.CoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WarNov.EasyPost/WarNov.EasyPost.NetStandard/EasyPost.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace WarNov.EasyPost.NetStandard
+{
+    public static class EasyPost
+    {
+        /// <summary>
+        /// Sends a post to a given open URL (no auth headers - Maybe the auth could be a token in the url: as Azure Functions work)
+        /// </summary>
+        /// <param name="url">The URL to send the info</param>
+        /// <param name="payload">The info to be sent</param>
+        /// <returns>The HttpResponseMessage with the result of the operation</returns>
+        public static HttpResponseMessage Post(string url, object payload)
+        {           
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+                return client.PostAsync(url, content).Result;
+            }           
+        }            
+    }
+}
